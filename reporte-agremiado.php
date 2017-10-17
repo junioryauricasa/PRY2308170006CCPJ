a30caa700841928a2b4e8714d9fee76737336607
elShunior <zahoz.moopa@gmail.com>
2017-10-17 17:50:18 -0500

    TILDES

--- reporte-agremiado.php
+++ reporte-agremiado.php
@@ -80,7 +80,7 @@ $codigoHTML='
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utF-8">/>
   <title>Reporte</title>
   <link href="https://fonts.googleapis.com/css?family=Slabo+27px&amp;subset=latin-ext" rel="stylesheet">
 </head>
